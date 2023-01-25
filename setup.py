68a4c7e5bcfc47647d5d6cf22df4c5c8b7ecc1d3
pyy <pyy.whoami@foxmail.com>
2023-01-25 19:53:58 +0800

    try fix

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
   name='pitricks',
-  version='0.0.1',
+  version='0.0.2',
   author='pyy',
   url='https://github.com/one-pyy/pitricks',
   py_modules=['pitricks'],
   install_requires=[],
-  description='python so hard'
+  description='python真难Q皿Q'
 )
