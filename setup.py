5cb815b772cbdc3b3415125a0c3ccf093720b150
pyy <pyy.whoami@foxmail.com>
2023-01-25 20:05:02 +0800

    更改安装包

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
   name='pitricks',
-  version='0.0.2',
+  version='0.0.4',
   author='pyy',
   url='https://github.com/one-pyy/pitricks',
-  py_modules=['pitricks'],
+  py_modules=find_packages(where='.', exclude=(), include=('*',)),
   install_requires=[],
   description='python真难Q皿Q'
 )
