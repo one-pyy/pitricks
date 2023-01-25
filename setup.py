05a7452426699650f5fbd79b538de56cae2807a3
pyy <pyy.whoami@foxmail.com>
2023-01-25 20:12:44 +0800

    尝试修复安装失败

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
-
+print(find_packages(where='.'))
 setup(
   name='pitricks',
   version='0.0.4',
   author='pyy',
   url='https://github.com/one-pyy/pitricks',
-  py_modules=find_packages(where='.', exclude=(), include=('*',)),
+  py_modules=find_packages(where='.'),
   install_requires=[],
   description='python真难Q皿Q'
 )
