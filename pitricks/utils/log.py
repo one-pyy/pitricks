d86b7d17e86c6c4e9ae4243a56cce234c9eed1f9
pyy <0vk.QxQ@pm.me>
2023-04-01 11:55:37 +0800

    加入一点rich

--- pitricks/utils/log.py
+++ pitricks/utils/log.py
@@ -1,19 +1,30 @@
 import logging
+from rich.logging import RichHandler
 
 __all__ = ['init_log']
 
-def init_log(filename: str = ""):
+def init_log(level, filename: str = ""):
   """自用初始化log"""
   logging.root.setLevel(logging.NOTSET)
-  formatter = logging.Formatter(
-    fmt='%(asctime)s [%(levelname)s] %(filename)s,%(lineno)d:%(funcName)s    %(message)s',
-    datefmt="%Y-%m-%d %H:%M:%S")
   
   if filename:
+    file_formatter = logging.Formatter(
+      fmt='%(asctime)s [%(levelname)s] %(pathname)s,%(lineno)d:%(funcName)s    %(message)s',
+      datefmt="%Y-%m-%d %H:%M:%S")
     file_handler = logging.FileHandler(filename, encoding='UTF-8')
-    file_handler.setFormatter(formatter)
+    file_handler.setLevel(level)
+    file_handler.setFormatter(file_formatter)
     logging.root.addHandler(file_handler)
   
-  stream_handler = logging.StreamHandler()
-  stream_handler.setFormatter(formatter)
-  logging.root.addHandler(stream_handler)
+  rich_formatter = logging.Formatter(
+    fmt='%(asctime)s [%(levelname)s] %(pathname)s,%(lineno)d: %(funcName)s\n%(message)s',
+    datefmt="%H:%M:%S")
+  rich = RichHandler(level=level,
+                     show_level=False,
+                     show_time=False,
+                     locals_max_string=None,
+                     show_path=False,
+                     rich_tracebacks=True,
+                     tracebacks_show_locals=True)
+  rich.setFormatter(rich_formatter)
+  logging.root.addHandler(rich)
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 setup(
   name='pitricks',
-  version='0.1.1-alpha.1',
+  version='0.1.2',
   author='pyy',
   url='https://github.com/one-pyy/pitricks',
   packages=find_packages(),
-  requires=['regex'],
+  requires=['regex', 'rich'],
   description='a tool box'
 )
