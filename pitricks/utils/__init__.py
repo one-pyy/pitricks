667e0c07ac52b574fe485cc353791055a38f89b0
pyy <0vk.QxQ@pm.me>
2023-04-03 11:57:51 +0800

    修改import

--- pitricks/utils/__init__.py
+++ pitricks/utils/__init__.py
@@ -1,9 +1,9 @@
-from .retry import *
-from .sync import *
-from .log import *
-from .reflect import *
-from .test import *
-from .handle_exp import *
-from .code_generater import *
+from .retry import retry, aretry
+from .sync import run_sync, patch_asyncio
+from .log import init_log
+from .reflect import get_upper_vars, get_args, pwd
+from .test import get_random_str
+from .handle_exp import catch_exp, acatch_exp
+from .code_generater import CodeGenerater
 from .use_pool import use_pool
 from .relative_import_everywhere import make_parent_top
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 setup(
   name='pitricks',
-  version='0.1.2',
+  version='0.1.3a2',
   author='pyy',
   url='https://github.com/one-pyy/pitricks',
   packages=find_packages(),
-  requires=['regex', 'rich'],
+  requires=['regex', 'rich', 'nest_asyncio'],
   description='a tool box'
 )
